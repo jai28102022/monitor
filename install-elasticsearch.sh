561981f156cfa8d905a8166de53ade183bd766f8
jai28102022 <116903411+jai28102022@users.noreply.github.com>
2022-11-10 12:04:22 +0530

    Update install-elasticsearch.sh

--- install-elasticsearch.sh
+++ install-elasticsearch.sh
@@ -2,7 +2,7 @@ curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt update
 sudo apt install elasticsearch -y
-cat  /etc/elasticsearch/elasticsearch.yml
+sudo cat  /etc/elasticsearch/elasticsearch.yml
 echo "network.host: localhost" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 sudo cat /etc/elasticsearch/elasticsearch.yml
 sudo systemctl start elasticsearch
