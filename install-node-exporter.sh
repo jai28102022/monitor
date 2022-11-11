aeae54554ac7fa8ccdbb03c0f392ddbe49cf4182
jai28102022 <116903411+jai28102022@users.noreply.github.com>
2022-11-11 11:49:21 +0530

    Update install-node-exporter.sh

--- install-node-exporter.sh
+++ install-node-exporter.sh
@@ -11,6 +11,13 @@ sudo systemctl status node_exporter
 sudo systemctl enable node_exporter
 
 cat <<EOF | sudo tee /etc/prometheus/prometheus.yml 
+global:
+  scrape_interval: 15s
+scrape_configs:
+  - job_name: 'prometheus'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9090']
   - job_name: 'node_exporter'
     scrape_interval: 5s
     static_configs:
